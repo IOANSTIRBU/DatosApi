d6baf8b0c4ebb1b782c016ded5dd57da08024245
iostirbu <ioan.stirbu@emeal.nttdata.com>
2023-10-03 12:02:42 +0200

    createClass update

---
+++ createClass.js
@@ -0,0 +1,30 @@
+class Mascota {
+  constructor(id, name) {
+    this.id = id;
+    this.name = name;
+  }
+}
+
+// Método estático para contar elementos con el mismo nombre
+Mascota.contarMascotas = function (itemArray, nameToCount) {
+  let count = 0;
+  for (const item of itemArray) {
+    if (item.name === nameToCount) {
+      count++;
+    }
+  }
+  return count;
+};
+
+// Ejemplo de uso de la clase
+const items = [
+  new Mascota(1, "William"),
+  new Mascota(2, "Floyd"),
+  new Mascota(3, "William"),
+  new Mascota(4, "Tino"),
+];
+
+const nameToCount = "William";
+const count = Mascota.contarMascotas(items, nameToCount);
+
+console.log(`Cantidad de mascotas con nombre igual"${nameToCount}": ${count}`);
